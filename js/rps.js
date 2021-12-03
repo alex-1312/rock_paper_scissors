f9a403d4c41610b4089054f2bbcc1b01f4d3ec47
alex-1312 <alexroth@mail.de>
2021-12-03 20:58:15 +0100

    added function playRound()

--- js/rps.js
+++ js/rps.js
@@ -15,5 +15,31 @@ function computerPlay(){
   }
   return result;
 }
+// lets play a single round rps
+function playRound( playerSelection, computerSelection){
+  let ps = playerSelection;
+  let cs = computerSelection;
+  
+  // TODO:  paper and scissors
+  //        catching user input
+
+  if ( ps == 'rock' && cs == 'Rock' ) {
+    return 'It\'s a draw! Rock vs Rock.';
+  } else if ( ps == 'rock' && cs == 'Paper' ) {
+    return 'You lose! Paper beats Rock.';
+  } else if ( ps == 'rock' && cs == 'Scissor' ) {
+    return 'You won! Rock beats Scissor.';
+  }
+}
+
+
+// TESTING STUFF
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
 
 // console.log(computerPlay());
+// console.log('const playerSelection: ' + playerSelection);
+// console.log('const computerSelection: ' + computerSelection);
