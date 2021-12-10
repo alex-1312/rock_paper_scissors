9e259dd4cae2e09784d3ab10ef7a7dd91e8896ca
alex-1312 <alexroth@mail.de>
2021-12-10 19:53:54 +0100

    added ui

--- js/rps.js
+++ js/rps.js
@@ -5,7 +5,7 @@ function capitalize( str ){
   } else {
     return '';
   }
-}
+};
 
 // function randomly returns rock, paper or scissors
 function computerPlay(){
@@ -23,38 +23,85 @@ function computerPlay(){
       break;
   }
   return result;
-}
+};
 
 function playRound( playerSelection, computerSelection){
   let ps = playerSelection;
   let cs = computerSelection;
   let err = 'Oops. Something went wrong!'; 
-
+  
   ps = ps.toLowerCase();
 
   if ( ps == cs ) { 
-    return `It\'s a draw! ${capitalize(ps)} vs ${capitalize(cs)}.`; 
+    combatText.textContent = `It\'s a draw! ${capitalize(ps)} vs ${capitalize(cs)}.`; 
   } else if ( ( ps == 'rock' && cs == 'paper' ) ||
             ( ps == 'scissor' && cs == 'rock' ) ||
-            ( ps == 'paper' && cs == 'scissor' ) ) { 
-      return `You lose! ${capitalize(cs)} beats ${capitalize(ps)}.`; 
+            ( ps == 'paper' && cs == 'scissor' ) ) {
+      computerScore++;
+      scoreParaContainer.textContent = computerScore;
+      combatText.textContent = `You lose! ${capitalize(cs)} beats ${capitalize(ps)}.`; 
   } else if ( ( ps == 'rock' && cs == 'scissor' )||
               ( ps == 'scissor' && cs == 'paper' ) ||
               ( ps == 'paper' && cs == 'rock' ) ) { 
-      return `You won! ${capitalize(ps)} beats ${capitalize(cs)}.`; 
+      playerScore++;
+      scoreParaPlayer.textContent = playerScore;
+      combatText.textContent = `You won! ${capitalize(ps)} beats ${capitalize(cs)}.`; 
   } else {
       return err;
   }
+  round++;
+  roundText.textContent = `Rounds: ${round}`;
+  if ( playerScore == 3 || computerScore == 3 ) {
+    let msgPara = document.createElement('p');
+    msgPara.textContent = 'GAME OVER!';
+    let btnReset = document.createElement('button');
+    btnReset.textContent = 'New Game';
+    btnReset.classList.add('btn');
+    btnReset.addEventListener('click', newGame);
+    
+    if ( playerScore == 3 ) {
+      combatText.textContent = `You won ${playerScore} out of ${round} rounds. Your opponent scored ${computerScore}.`;
+    } else {
+      combatText.textContent = `You lost! You got ${playerScore} points out of ${round} rounds. Your opponent scored ${computerScore}.`;
+    }
+
+    combatText.appendChild(msgPara);
+    combatText.appendChild(btnReset);
+
+    // remove eventListener from btn's
+    document.getElementById('btn-rock').removeEventListener('click', rock);
+    document.getElementById('btn-paper').removeEventListener('click', paper);
+    document.getElementById('btn-scissor').removeEventListener('click', scissor);
+  }
+};
+
+function rock() {
+  playRound('rock', computerPlay());
+};
+
+function paper() {
+  playRound('paper', computerPlay());
+};
+
+function scissor() {
+  playRound('scissor', computerPlay());
+};
+
+function newGame(){
+  location.reload();
 }
-// play the game 5 times in a row
-// function game ( ){  
-//   for ( let i = 1; i <= 5; i++ ){
-//     // console.log('Loop: ' + i);
-//     let ps = prompt('Your Choise. Rock, Paper or Scissor');
-//     let cs = computerPlay();
-//     console.log('Loop ' + i + ': ' + playRound(ps, cs));
-//   }
-// } 
+// ########################################################################
+
+// vars
+let computerScore = 0;
+let playerScore = 0;
+let round = 0;
+
+// container
+const scoreParaPlayer = document.querySelector('#ps');
+const scoreParaContainer = document.querySelector('#cs');
+const combatText = document.querySelector('#combat-text');
+const roundText = document.querySelector('#round');
 
 // button eventListener 
 // variables
@@ -62,16 +109,8 @@ const btnRock = document.querySelector('#btn-rock');
 const btnPaper = document.querySelector('#btn-paper');
 const btnScissor = document.querySelector('#btn-scissor');
 // events
-btnRock.addEventListener('click', () => {
-  console.log(playRound('rock', computerPlay()));
-});
-btnPaper.addEventListener('click', () => {
-  console.log(playRound('paper', computerPlay()));
-});
-btnScissor.addEventListener('click', () => {
-  console.log(playRound('Scissor', computerPlay()));
-});
+btnRock.addEventListener('click', rock);
+btnPaper.addEventListener('click', paper);
+btnScissor.addEventListener('click', scissor);
 
 // TESTING STUFF
-// console.log(capitalize('hello'));
-// console.log(playRound('rock',computerPlay()));
